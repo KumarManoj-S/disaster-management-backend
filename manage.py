da3c1f8d1f60c3f2fa81d26fc379a37bda73352d
Manoj Kumar S <manojkumars@sahajsoft.com>
2020-05-24 01:09:17 +0530

    Specify port

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from app.main.api import blueprint
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 cors = CORS(app)
 
 if __name__ == '__main__':
     app.register_blueprint(blueprint)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
